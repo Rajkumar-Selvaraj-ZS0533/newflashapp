f917109898642817a32f8e9fb95bea4f86562eb6
Rajkumar-Selvaraj <148527647+Rajkumar-Selvaraj@users.noreply.github.com>
2024-02-04 17:27:56 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,17 @@
-from flask import Flask, send_file
+from flask import Flask, render_template, jsonify
 import pandas as pd
 import matplotlib.pyplot as plt
 from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
+    return render_template('dashboard.html')
+
+@app.route('/api/charts_data')
+def charts_data():
     # Read data from Excel file
     excel_file_path = 'data/Demo.xlsx'  # Replace with the actual path to your Excel file
     df = pd.read_excel(excel_file_path)
@@ -16,20 +21,33 @@ def home():
     values = df['Value'].tolist()
 
     # Create a bar chart
-    plt.bar(categories, values)
+    bar_chart = create_bar_chart(categories, values)
 
-    # Save the plot to a BytesIO object
+    # Create a pie chart
+    pie_chart = create_pie_chart(categories, values)
+
+    return jsonify({
+        'bar_chart': bar_chart,
+        'pie_chart': pie_chart
+    })
+
+def create_bar_chart(categories, values):
+    plt.bar(categories, values)
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
     plt.close()
+    chart_data = base64.b64encode(buffer.read()).decode('utf-8')
+    return chart_data
 
-    # Return the BytesIO object as an image file
-    return send_file(buffer, download_name='chart.png', mimetype='image/png')
-
-@app.route('/about')
-def about():
-    return 'About'
+def create_pie_chart(categories, values):
+    plt.pie(values, labels=categories, autopct='%1.1f%%')
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    chart_data = base64.b64encode(buffer.read()).decode('utf-8')
+    return chart_data
 
 if __name__ == '__main__':
     app.run(debug=True)
